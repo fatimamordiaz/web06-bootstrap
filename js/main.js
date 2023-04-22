7f7ea1906e055d86f7dbd28c5da3da4a5c6b34e0
Fatima Moreno <fatimamordiaz@gmail.com>
2023-04-22 13:58:28 -0600

    se corrigen elementos del reset

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ function changeColorToRed(){
     const refName = document.getElementById("name");
     refName.style.color = "red"
 }
+function changeColorToBlue(){
+    const refName = document.getElementById("name");
+    refName.style.color = "blue"
+}
+function changeColorToGreen(){
+    const refName = document.getElementById("name");
+    refName.style.color = "green"
+}
+function changeColorParagraph(color, refObj){
+   console.log(refObj);
+    refObj.style.color = color;
+}
+// esta primera es para resetear color
+function resetColor () {
+    const colorCollection = document.getElementsByClassName ("text-color");
+    console.log(colorCollection);
+    for (let i = 0; i < colorCollection.length; i++) {
+        const element = colorCollection [i];
+        element.style.color = "black";
+    }
+
+}
